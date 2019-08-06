c6bd5846cc84ea73ef43035906d2dfee0e0e11b0
malekz <malek.zarzour@gmail.com>
2019-08-06 16:38:21 +0200

    created list that holds cards

---
+++ js/app.js
@@ -0,0 +1,21 @@
+/*
+ * Create a list that holds all of your cards
+ */
+const icons = [
+  "fa fa-anchor",
+  "fa fa-bolt",
+  "fa fa-bomb",
+  "fa fa-bicycle",
+  "fa fa-cube",
+  "fa fa-diamond",
+  "fa fa-leaf",
+  "fa fa-paper-plane-o",
+  "fa fa-anchor",
+  "fa fa-bolt",
+  "fa fa-bomb",
+  "fa fa-bicycle",
+  "fa fa-cube",
+  "fa fa-diamond",
+  "fa fa-leaf",
+  "fa fa-paper-plane-o"
+];
