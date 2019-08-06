5defcc1524cd3ac1f5c03cac03bbbb219b26e9dc
malekz <malek.zarzour@gmail.com>
2019-08-06 16:46:27 +0200

    created shuffle function

--- js/app.js
+++ js/app.js
@@ -50,3 +50,23 @@ const btnModal = document.querySelector(".btn-modal");
 
 let checkCards = [];
 let matchedCards = [];
+
+////////////////////////////////////////////////////////////////////////
+// Shuffle function from http://stackoverflow.com/a/2450976//
+/////////////////////////////////////////////////////////////////////
+
+function shuffle(array) {
+  var currentIndex = array.length,
+    temporaryValue,
+    randomIndex;
+
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
