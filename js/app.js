874a63d669963f8ac3b2bdf906a5a003f1baf29d
malekz <malek.zarzour@gmail.com>
2019-08-06 16:39:20 +0200

    configured the game globak variables

--- js/app.js
+++ js/app.js
@@ -19,3 +19,27 @@ const icons = [
   "fa fa-leaf",
   "fa fa-paper-plane-o"
 ];
+
+/////////////////////////////
+//// Global Variables ////
+//////////////////////////
+
+// cards board
+const cardsBoard = document.querySelector("#cards-board");
+
+// Moves
+let moves = 0;
+const movesCounter = document.querySelector(".moves");
+
+// Rating
+const stars = document.querySelector(".stars").childNodes;
+const starsForRate = document.querySelector(".stars");
+
+// Restart
+const restart = document.querySelector(".restart");
+
+// Modal
+const modal = document.querySelector(".modal");
+const ratingModal = document.querySelector(".rating-modal");
+const movesModal = document.querySelector(".moves-modal");
+const btnModal = document.querySelector(".btn-modal");
