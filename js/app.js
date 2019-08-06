a5acd2004e83ea1b05f3e0430ed321ade60947fc
malekz <malek.zarzour@gmail.com>
2019-08-06 16:48:41 +0200

    card borad

--- js/app.js
+++ js/app.js
@@ -70,3 +70,185 @@ function shuffle(array) {
 
   return array;
 }
+
+//////////////////////////////////////////////////////////////////
+
+creatCardsBoard();
+
+///////////////////////////////////////////////////////////////
+/////// Deck Creation and adding EventListener ////////
+////////////////////////////////////////////////////////////
+
+function creatCardsBoard() {
+  // To clear the old card board
+  cardsBoard.innerHTML = "";
+  // creat new ul element to append it to "cardsBoard"
+  const myNewDeck = document.createElement("ul");
+  myNewDeck.classList.add("deck");
+  // shuffle the icons list
+  let shufIcons = shuffle(icons);
+  for (let i = 0; i < shufIcons.length; i++) {
+    const newLi = document.createElement("li");
+    newLi.classList.add("card");
+    newLi.innerHTML = `<i class="${shufIcons[i]}"></i>`;
+    myNewDeck.appendChild(newLi);
+  }
+  cardsBoard.append(myNewDeck);
+  // add event listener to the cards board
+  cardsBoard.addEventListener("click", respondToTheClick);
+}
+
+///////////////////////////
+//// Opening Cards ///
+////////////////////////
+
+function respondToTheClick(e) {
+  let selectedCard = e.target;
+  // to make sure that the clicked target is a card &
+  // not an opened/matched card
+  if (
+    selectedCard.classList.contains("card") &&
+    !selectedCard.classList.contains("open", "show", "match")
+  ) {
+    // add classes open and show to the selected card
+    selectedCard.classList.add("open", "show");
+    // add the selected card to checkCards array to check if it's
+    // like the next selected card or not
+    checkCards.push(selectedCard);
+  }
+  // checking cards when their are two cards in checkCards array
+  if (checkCards.length === 2) {
+    // to prevent opening more than two cards till the
+    // checking process is finished
+    cardsBoard.classList.add("stop-event");
+    //counting the moves
+    movesNum();
+    // if the cards are matched call the matched function
+    if (checkCards[0].innerHTML === checkCards[1].innerHTML) {
+      matched();
+    } else {
+      // if they aren't matched call the notMatched function
+      // after 800ms to allow the player to see the second card
+      setTimeout(notMatched, 800);
+    }
+    wohoo();
+  }
+}
+
+// if the cards are matched
+function matched() {
+  // add class match to both cards
+  checkCards[0].classList.add("match");
+  checkCards[1].classList.add("match");
+  // push both cards to the matchedCards array
+  matchedCards.push(checkCards[0]);
+  matchedCards.push(checkCards[1]);
+  // remove cards from checkCards array
+  checkCards = [];
+  // to allow opening and checking two cards again
+  cardsBoard.classList.remove("stop-event");
+}
+// if the cards are not matched
+function notMatched() {
+  // remove open & show classes from both cards
+  checkCards[0].classList.remove("open", "show");
+  checkCards[1].classList.remove("open", "show");
+  // remove cards from checkCards array
+  checkCards = [];
+  // to allow opening and checking two cards again
+  cardsBoard.classList.remove("stop-event");
+}
+
+///////////////////////////////
+////////// Moves ///////////
+////////////////////////////
+
+function movesNum() {
+  // to increment moves number after opening two cards
+  moves++;
+  if (moves === 1) {
+    movesCounter.innerHTML = `1  Move`;
+  } else {
+    movesCounter.innerHTML = `${moves}  Moves`;
+  }
+  starsRating();
+}
+
+////////////////////////////////////
+///////// Stars Rating ///////////
+//////////////////////////////////
+
+function starsRating() {
+  // if the moves number is between 12 and 19
+  if (moves === 12) {
+    // change the color of the third star to grey
+    stars[5].classList.add("grey");
+    // if the moves number is 20 or more
+  } else if (moves === 20) {
+    // change the color of the second star to grey
+    stars[3].classList.add("grey");
+  }
+}
+
+////////////////////////////////////////////////////////
+/////////////////// Restart Game ///////////////////
+////////////////////////////////////////////////////
+
+function restartGame() {
+  // set it to false in order to fulfil the condition
+  // at line 130 to start the timer after opening the
+  // first card after restarting the game
+  // reset the moves to zero
+  moves = 0;
+  movesCounter.innerHTML = `0 Moves`;
+  // empty both arrays
+  matchedCards = [];
+  checkCards = [];
+  // to clear the old board, create a new
+  // shuffled  cards board
+  creatCardsBoard();
+
+  // reset the color of the stars
+  stars[5].classList.remove("grey");
+  stars[3].classList.remove("grey");
+}
+// to restart  the game when the player click on the restart icon
+restart.addEventListener("click", restartGame);
+
+////////////////////////////////////////////////////////
+/////////////// Win Modal Function ///////////////
+////////////////////////////////////////////////////
+
+function wohoo() {
+  //when the player finish the game
+  if (matchedCards.length === 16) {
+    ratingModal.innerHTML = starsForRate.innerHTML;
+    movesModal.innerHTML = movesCounter.innerHTML.slice(0, 3);
+    modal.style.display = "block";
+  }
+}
+
+btnModal.addEventListener("click", function() {
+  // to close the modal and restart the game
+  modal.style.display = "none";
+  restartGame();
+});
+
+////////////////////////////////////////////////////
+/*
+ * Display the cards on the page
+ *   - shuffle the list of cards using the provided "shuffle" method below
+ *   - loop through each card and create its HTML
+ *   - add each card's HTML to the page
+ */
+
+/*
+ * set up the event listener for a card. If a card is clicked:
+ *  - display the card's symbol (put this functionality in another function that you call from this one)
+ *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
+ *  - if the list already has another card, check to see if the two cards match
+ *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
+ *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
+ *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
+ *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+ */
